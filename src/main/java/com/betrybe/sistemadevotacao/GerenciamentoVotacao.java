e4d6c756bb46ac71842088e4f4c4dc1e3ef987eb
lucopdev <lucopdev@gmail.com>
2023-10-27 17:36:40 -0300

    req 4

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,27 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+/**
+ * The type Gerenciamento votacao.
+ */
+public abstract class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+
+  private ArrayList pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  private ArrayList cpfsComputados = new ArrayList<String>();
+
+  /**
+   * Instantiates a new Gerenciamento votacao.
+   *
+   * @param pessoaCandidata the pessoa candidata
+   * @param pessoaEleitora  the pessoa eleitora
+   * @param cpfComputado    the cpf computado
+   */
+  public GerenciamentoVotacao(PessoaCandidata pessoaCandidata,
+      PessoaEleitora pessoaEleitora, String cpfComputado) {
+    this.pessoasCandidatas.add(pessoaCandidata);
+    this.pessoasEleitoras.add(pessoaEleitora);
+    this.cpfsComputados.add(cpfComputado);
+  }
 }

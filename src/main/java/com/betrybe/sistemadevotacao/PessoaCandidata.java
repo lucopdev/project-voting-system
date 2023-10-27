2ebe2823ac9ad6d81bf700f4836080de931091ce
lucopdev <lucopdev@gmail.com>
2023-10-27 16:10:28 -0300

    req 2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,59 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * The type Pessoa candidata.
+ */
+public class PessoaCandidata extends Pessoa {
 
+  /**
+   * The Numero.
+   */
+  public int numero;
+  private int votos;
+
+  /**
+   * Instantiates a new Pessoa candidata.
+   *
+   * @param nome   the nome
+   * @param numero the numero
+   */
+  public PessoaCandidata(String nome, int numero) {
+    super.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  /**
+   * Receber voto.
+   */
+  public void receberVoto() {
+    this.votos += 1;
+  }
+
+  /**
+   * Gets votos.
+   *
+   * @return the votos
+   */
+  public int getVotos() {
+    return votos;
+  }
+
+  /**
+   * Gets numero.
+   *
+   * @return the numero
+   */
+  public int getNumero() {
+    return numero;
+  }
+
+  /**
+   * Sets numero.
+   *
+   * @param numero the numero
+   */
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
 }

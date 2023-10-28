661f81ae7da30d391ecee7f57041791e3fcb2962
lucopdev <lucopdev@gmail.com>
2023-10-27 23:28:55 -0300

    req 5

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -5,23 +5,73 @@ import java.util.ArrayList;
 /**
  * The type Gerenciamento votacao.
  */
-public abstract class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
-  private ArrayList pessoasCandidatas = new ArrayList<PessoaCandidata>();
-  private ArrayList pessoasEleitoras = new ArrayList<PessoaEleitora>();
-  private ArrayList cpfsComputados = new ArrayList<String>();
+  final private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
+  final private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
+  final private ArrayList<String> cpfsComputados = new ArrayList<>();
 
-  /**
-   * Instantiates a new Gerenciamento votacao.
-   *
-   * @param pessoaCandidata the pessoa candidata
-   * @param pessoaEleitora  the pessoa eleitora
-   * @param cpfComputado    the cpf computado
-   */
-  public GerenciamentoVotacao(PessoaCandidata pessoaCandidata,
-      PessoaEleitora pessoaEleitora, String cpfComputado) {
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata pessoasCandidata : this.pessoasCandidatas) {
+      boolean numeroEncontrado = pessoasCandidata.numero == numero;
+
+      if (numeroEncontrado) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+    PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
     this.pessoasCandidatas.add(pessoaCandidata);
+  }
+
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora pessoasEleitora : this.pessoasEleitoras) {
+      boolean cpfEncontrado = pessoasEleitora.getCpf().equals(cpf);
+
+      if (cpfEncontrado) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+    PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
     this.pessoasEleitoras.add(pessoaEleitora);
-    this.cpfsComputados.add(cpfComputado);
+  }
+
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    for (String cpfsComputado : cpfsComputados) {
+      boolean cpfEncontrado = cpfsComputado.equals(cpfPessoaEleitora);
+
+      if (cpfEncontrado) {
+        System.out.println("Pessoa eleitora já votou!");
+        return;
+      }
+    }
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      boolean numeroEncontrado = pessoaCandidata.numero == numeroPessoaCandidata;
+
+      if (numeroEncontrado) {
+        cpfsComputados.add(cpfPessoaEleitora);
+        pessoaCandidata.receberVoto();
+      }
+    }
+  }
+
+  public void mostrarResultado() {
+    if (!cpfsComputados.isEmpty()) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado");
+    }
+
+    int totalVotos = 0;
+
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      String nome = pessoaCandidata.getNome();
+      int votos = pessoaCandidata.getVotos();
+      totalVotos += votos;
+      System.out.println(
+          "Nome: " + nome + " - " + votos + " votos " + "( " + Math.round(
+              (votos * 100) / totalVotos)
+              + "% )");
+    }
+    System.out.println(totalVotos);
   }
 }
--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -1,10 +1,85 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.Scanner;
+
+/**
+ * The type Principal.
+ */
 public class Principal {
 
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
+    GerenciamentoVotacao votacao = new GerenciamentoVotacao();
 
+    Scanner scanner = new Scanner(System.in);
+
+    int opcao = 0;
+
+    while (opcao != 2) {
+      System.out.println("Cadastrar pessoa candidata?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.print("Entre com o número correspondente à opção desejada: ");
+      opcao = scanner.nextInt();
+
+      if (opcao == 1) {
+        System.out.print("Entre com o nome da pessoa candidata: ");
+        String nomeCandidato = scanner.next();
+
+        System.out.print("Entre com o número da pessoa candidata: ");
+        int numeroCandidato = scanner.nextInt();
+
+        votacao.cadastrarPessoaCandidata(nomeCandidato, numeroCandidato);
+      }
+    }
+
+    opcao = 0;
+
+    while (opcao != 2) {
+      System.out.println("Cadastrar pessoa eleitora?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.print("Entre com o número correspondente à opção desejada: ");
+      opcao = scanner.nextInt();
+
+      if (opcao == 1) {
+        System.out.print("Entre com o nome da pessoa eleitora: ");
+        String nomeEleitor = scanner.next();
+        System.out.print("Entre com o cpf da pessoa candidata: ");
+        String cpfEleitor = scanner.next();
+
+        votacao.cadastrarPessoaEleitora(nomeEleitor, cpfEleitor);
+      }
+    }
+
+    opcao = 0;
+
+    while (opcao != 3) {
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      System.out.println("1 - Votar");
+      System.out.println("2 - Resultado Parcial");
+      System.out.println("3 - Finalizar Votação");
+      opcao = scanner.nextInt();
+
+      if (opcao == 1) {
+        System.out.print("Entre com o cpf da pessoa eleitora: ");
+        String cpfEleitorVotacao = scanner.next();
+        System.out.print("Entre com o numero da pessoa candidata: ");
+        int numeroCandidatoVotacao = scanner.nextInt();
+
+        votacao.votar(cpfEleitorVotacao, numeroCandidatoVotacao);
+      }
+
+      if (opcao == 2) {
+        votacao.mostrarResultado();
+      }
+    }
+    votacao.mostrarResultado();
+    scanner.close();
   }
-
 }
